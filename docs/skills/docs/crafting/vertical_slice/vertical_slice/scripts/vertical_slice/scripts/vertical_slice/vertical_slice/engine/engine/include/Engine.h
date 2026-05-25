96ffbd699d34e45d3f0cc817eb31fa6e7bbc89a3
Brenden <sk.h8er.boi113@gmail.com>
2026-05-24 23:28:11 -0700

    Update Engine.h

--- docs/skills/docs/crafting/vertical_slice/vertical_slice/scripts/vertical_slice/scripts/vertical_slice/vertical_slice/engine/engine/include/Engine.h
+++ docs/skills/docs/crafting/vertical_slice/vertical_slice/scripts/vertical_slice/scripts/vertical_slice/vertical_slice/engine/engine/include/Engine.h
@@ -8,37 +8,6 @@ struct GLFWwindow;
 namespace engine {
 
 class Renderer;
-
-class Engine {
-public:
-  Engine();
-  ~Engine();
-
-  bool Initialize();
-  void Tick(float dt);
-  void Shutdown();
-
-  std::string GetName() const;
-
-private:
-  bool initialized_;
-  std::string name_;
-
-  GLFWwindow* window_;
-  std::unique_ptr<Renderer> renderer_;
-};
-
-} // namespace engine
-#pragma once
-
-#include <string>
-#include <memory>
-
-struct GLFWwindow;
-
-namespace engine {
-
-class Renderer;
 class Camera;
 
 class Engine {
