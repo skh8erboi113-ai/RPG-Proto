69e2906514c2009f698af40f0e9c9344d56123ff
Brenden <sk.h8er.boi113@gmail.com>
2026-05-24 23:19:43 -0700

    Create Camera.h

---
+++ engine/include/Camera.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+
+namespace engine {
+
+class Camera {
+public:
+  Camera();
+
+  void SetPerspective(float fovYRadians, float aspect, float zNear, float zFar);
+  void SetPosition(float x, float y, float z);
+  void LookAt(float targetX, float targetY, float targetZ);
+
+  const float* GetViewMatrix() const { return view_; }
+  const float* GetProjMatrix() const { return proj_; }
+
+private:
+  float pos_[3];
+  float target_[3];
+  float view_[16];
+  float proj_[16];
+};
+
+} // namespace engine
